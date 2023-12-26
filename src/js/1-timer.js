6ee11ee0d5b17f42eb72c6bb6e4605bfa9db09de
anyalenzy <anya.dudnik.91@gmail.com>
2023-12-26 18:11:07 +0200

    done task-2

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,7 +1,5 @@
 import flatpickr from 'flatpickr';
-import 'flatpickr/dist/flatpickr.min.css';
 import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
 
 const startBtn = document.querySelector('[data-start]'),
   dataDays = document.querySelector('span[data-days]'),
